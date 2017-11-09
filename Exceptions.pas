7f5ff28badd27f03f3972f79a2c18669f0267bfa
John Moshakis <JohnMoshakis@john-moshakiss-macbook-pro.local>
2017-11-08 20:20:28 -0500

    WebProxy class and some exceptions

---
+++ Exceptions.pas
@@ -0,0 +1,67 @@
+﻿namespace Moshine.Foundation;
+
+uses
+  Foundation;
+
+type
+
+  ProxyException = public class (NSException)
+  public
+    property Url:String;
+
+    method initWithName(aName: NSExceptionName) reason(aReason: nullable NSString) userInfo(aUserInfo: nullable NSDictionary):instancetype; override;
+    begin
+      self := inherited initWithName(aName) reason(aReason) userInfo(aUserInfo);
+      if(assigned(self))then
+      begin
+        self.Url := '';
+
+      end;
+      exit self;
+    end;
+
+
+
+    method initWithName(name: not nullable NSString) reason(reason:String) userInfo(info:NSDictionary) FromUrl(fromUrl:NSString) :id;
+    begin
+      self := inherited initWithName(name) reason(reason) userInfo(info);
+      if(assigned(self))then
+      begin
+        self.Url := fromUrl;
+      end;
+
+      exit self;
+
+    end;
+
+  end;
+
+  HttpStatusCodeException = public class(ProxyException)
+  public
+    property StatusCode:Integer;
+
+    method initWithName(name: not nullable NSString) reason(reason:NSString) userInfo(info:NSDictionary) StatusCode(code:Integer) FromUrl(fromUrl:NSString) :id;
+    begin
+      self := inherited initWithName(name) reason(reason) userInfo(info);
+      if(assigned(self))then
+      begin
+        self.StatusCode := code;
+        self.Url := fromUrl;
+      end;
+      exit self;
+    end;
+
+  end;
+
+  AuthenticationRequiredException = public class(ProxyException)
+  end;
+
+  MissingAuthTokenException = public class(AuthenticationRequiredException)
+  end;
+
+
+  // No network connection, no local user so unable to proceed
+  NoNetworkConnectionException = public class(ProxyException)
+  end;
+
+end.
---
+++ WebProxy.pas
@@ -0,0 +1,170 @@
+﻿namespace Moshine.Foundation.Web;
+
+uses
+  Foundation,
+  Moshine.Foundation,
+  RemObjects.Elements.RTL;
+
+type
+
+  RequestBuilderDelegate = public block(url:String;webMethod:String;addAuthentication:Boolean):NSMutableURLRequest;
+
+
+  WebProxy = public class
+
+  protected
+
+    _requestBuilder:RequestBuilderDelegate;
+
+
+    method WebRequest<T>(webMethod:NSString; url:NSString;addAuthentication:Boolean := true):T;
+    begin
+      exit WebRequest(webMethod,url,addAuthentication) as T;
+    end;
+
+    method WebRequest(webMethod:NSString; url:NSString;addAuthentication:Boolean := true):NSObject;
+    begin
+      exit WebRequest(webMethod, url, nil,addAuthentication);
+    end;
+
+    method WebRequest<T>(webMethod:NSString; url:NSString; jsonBody:NSData;addAuthentication:Boolean := true):T;
+    begin
+      exit WebRequest(webMethod, url, jsonBody, addAuthentication) as T;
+    end;
+
+    method WebRequest(webMethod:NSString; url:NSString; jsonBody:NSData;addAuthentication:Boolean := true):NSObject;
+    begin
+
+      NSLog('WebRequest Method %@ Url %@',webMethod,url);
+
+      var request:NSMutableURLRequest;
+
+      if(assigned(_requestBuilder))then
+      begin
+        request := _requestBuilder(url,webMethod,addAuthentication);
+      end
+      else
+      begin
+        request := new NSMutableURLRequest() withURL( new NSURL() withString( url ));
+        request.setHTTPMethod(webMethod);
+      end;
+
+
+      if(assigned(jsonBody))then
+      begin
+
+        var dataLength := jsonBody.length();
+        var length := NSString.stringWithFormat('%ld', dataLength);
+
+        request.setValue('application/json; charset=utf-8') forHTTPHeaderField('Content-Type');
+        request.setValue('application/json') forHTTPHeaderField('Accept');
+        request.setValue( length ) forHTTPHeaderField('Content-Length');
+
+        request.setHTTPBody(jsonBody);
+
+      end;
+
+      var taskError:NSError;
+      var reason:NSString := nil;
+      var statusCode:NSInteger := 0;
+      var blockData:NSObject := nil;
+
+      var outerExecutionBlock: NSBlockOperation := NSBlockOperation.blockOperationWithBlock(method() begin
+
+        var semaphore := dispatch_semaphore_create(0);
+
+        var session := NSURLSession.sharedSession; // or create your own session with your own NSURLSessionConfiguration
+
+        var task := session. dataTaskWithRequest(request) completionHandler(method (data:NSData; response:NSURLResponse; error:NSError)begin
+
+          if(not assigned(error))then
+          begin
+            NSLog('WebRequest Method %@ Url %@ returned Response',webMethod,url);
+
+            if(response is NSHTTPURLResponse)then
+            begin
+              var httpResponse := response as NSHTTPURLResponse;
+
+              statusCode := httpResponse.statusCode;
+
+              NSLog('UrlResponse Method %@ Url %@ Response %ld',webMethod,url,statusCode);
+
+              if(statusCode <> 200)then
+              begin
+                reason := NSString.stringWithFormat('Url  %@ returned Invalid Status Code %ld', url, statusCode);
+              end;
+
+            end;
+
+            if(String.IsNullOrEmpty(reason))then
+            begin
+
+              var jsonError:NSError;
+
+              blockData := NSJSONSerialization.JSONObjectWithData(data) options(NSJSONReadingOptions.NSJSONReadingMutableContainers ) error( var jsonError );
+
+              if(assigned(jsonError))then
+              begin
+                blockData := nil;
+                taskError := jsonError;
+                reason := 'error from JSONObjectWithData';
+              end;
+            end;
+          end
+          else
+          begin
+            taskError := error;
+          end;
+
+          dispatch_semaphore_signal(semaphore);
+        end);
+
+        task.resume;
+
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+
+      end);
+
+      var workerQueue := new NSOperationQueue();
+      workerQueue.addOperations([outerExecutionBlock]) waitUntilFinished(true);
+
+      if(not String.IsNullOrEmpty(reason))then
+      begin
+        if(statusCode = 0)then
+        begin
+          raise new ProxyException withName('ProxyException') reason(reason) userInfo(nil) FromUrl(url);
+        end
+        else
+        begin
+          if(statusCode = 401)then
+          begin
+            NSLog('%@','raising AuthenticationRequiredException from 401 statusCode');
+            raise new AuthenticationRequiredException withName('AuthenticationRequired') reason('authentication required from 401 statusCod') userInfo(nil) FromUrl(url);
+          end
+          else
+          begin
+            raise new HttpStatusCodeException withName('ProxyException') reason(reason) userinfo(nil) StatusCode(statusCode) FromUrl(url);
+          end;
+        end;
+      end;
+
+      if(assigned(taskError))then
+      begin
+        NSLog('dataTaskWithRequest returned error');
+
+        if(taskError.code = NSURLErrorUserCancelledAuthentication)then
+        begin
+          NSLog('%@','raising AuthenticationRequiredException');
+          raise new AuthenticationRequiredException withName('AuthenticationRequired') reason('authentication required') userInfo(nil) FromUrl(url);
+        end;
+        NSLog('%@','raising ProxyException');
+        raise new ProxyException withName('ProxyException') reason('error from request') userInfo(nil) FromUrl(url);
+      end;
+
+      exit blockData;
+
+    end;
+
+  end;
+
+end.
