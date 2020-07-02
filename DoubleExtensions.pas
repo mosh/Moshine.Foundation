ab37729004a9f3be6ef24b42c5879f7c6a50ccab
John Moshakis <john.moshakis@firmex.com>
2020-07-02 01:05:21 -0400

    Make into extension method

---
+++ DoubleExtensions.pas
@@ -0,0 +1,19 @@
+﻿namespace Moshine.Foundation.Shared;
+
+type
+
+  DoubleExtensions = public extension class(Double)
+  private
+  protected
+  public
+
+    {$IF TOFFEE}
+    method epochToDate:Foundation.NSDate;
+    begin
+      exit Foundation.NSDate.dateWithTimeIntervalSince1970(self);
+    end;
+    {$ENDIF}
+
+  end;
+
+end.
