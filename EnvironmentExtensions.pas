46460c823af5395dc9155d56ae08f97845d2ea04
John Moshakis <john@moshakis.com>
2021-12-02 23:15:54 -0500

    Helper

---
+++ EnvironmentExtensions.pas
@@ -0,0 +1,21 @@
+﻿namespace Moshine.Foundation.Shared;
+
+type
+  EnvironmentExtensions = public extension class(RemObjects.Elements.RTL.Environment)
+  private
+  protected
+  public
+    class method CommandLineArgs:array of String;
+    begin
+      {$IFDEF ECHOES}
+      exit System.Environment.GetCommandLineArgs;
+      {$ELSEIF TOFFEE OR DARWIN}
+      exit Foundation.NSProcessInfo.processInfo.arguments;
+      {$ELSE}
+      raise NotImplementedException;
+      {$ENDIF}
+
+    end;
+  end;
+
+end.
