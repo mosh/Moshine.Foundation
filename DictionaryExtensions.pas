8621078065ed5599cac1535a83dc561e2eff4f11
John Moshakis <john@moshakis.com>
2022-01-08 16:24:40 -0500

    ToDictionary for Island

---
+++ DictionaryExtensions.pas
@@ -0,0 +1,41 @@
+﻿namespace Moshine.Foundation;
+
+type
+
+{$IF ISLAND}
+
+
+  DictionaryExtensions = public extension class(Object)
+  public
+    method ToDictionary:Dictionary<String,Object>;
+    begin
+      var dict := new Dictionary<String,Object>;
+      try
+
+        var propertyInfos := typeOf(self).Properties.Where(a ->
+          not a.IsStatic and
+          assigned(a.ReadMethod) and assigned(a.WriteMethod) and
+          (a.Arguments.Count = 0)).ToList();
+
+        for each propertyInfo in propertyInfos do
+        begin
+          var propertyValue := propertyInfo.GetValue(self,[]);
+          dict.Add(propertyInfo.Name,propertyValue);
+        end;
+
+      except
+        on ex:Exception do
+        begin
+          writeLn($'{ex.Message}');
+          raise;
+        end;
+      end;
+      exit dict;
+    end;
+
+  end;
+
+{$ENDIF}
+
+
+end.
