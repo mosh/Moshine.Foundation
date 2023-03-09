07d51e49f6368194a1ea940a7e465f8527c27c9f
John Moshakis <john@moshakis.com>
2023-03-08 21:02:52 -0500

    NodaTime extensions

---
+++ LocalDateTimeExtensions.pas
@@ -0,0 +1,22 @@
+﻿namespace Moshine.Foundation;
+
+{$IFDEF ECHOES}
+
+uses
+  NodaTime, NodaTime.TimeZones;
+
+type
+  LocalDateTimeExtensions = public extension class(LocalDateTime)
+
+  public
+    method ToDateTimeOffset(offsetInHours:Integer):DateTimeOffset;
+    begin
+      var time := self.PlusHours(offsetInHours);
+      var tz := BclDateTimeZone.ForOffset(Offset.FromHours(offsetInHours));
+      var timeInZone := time.InZoneLeniently(tz);
+      exit timeInZone.ToDateTimeOffset;
+    end;
+
+  end;
+{$ENDIF}
+end.
---
+++ ProductVersion.pas
@@ -0,0 +1,15 @@
+﻿namespace Moshine.Foundation;
+
+{$IFDEF ECHOES}
+
+uses
+  system.Reflection;
+
+[assembly: AssemblyVersion('1.0.0')]
+[assembly: AssemblyFileVersion('1.0.0')]
+[assembly: AssemblyCopyright('Copyright © 2023')]
+[assembly: AssemblyCompany('Moshine')]
+[assembly: AssemblyProduct('Moshine Foundation')]
+{$ENDIF}
+
+end.
