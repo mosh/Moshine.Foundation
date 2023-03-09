38db77c3f75269ca004a79ce2249f4f346885623
John Moshakis <john@moshakis.com>
2023-03-08 22:14:13 -0500

    more extensions

---
+++ DateTimeOffsetExtensions.pas
@@ -0,0 +1,19 @@
+﻿namespace Moshine.Foundation;
+
+{$IFDEF ECHOES}
+
+uses
+  NodaTime;
+
+type
+  DateTimeOffsetExtensions = public extension class(DateTimeOffset)
+  public
+    method OffsetToLocalDateTime:LocalDateTime;
+    begin
+      var zoned := ZonedDateTime.FromDateTimeOffset(self);
+      exit zoned.LocalDateTime.PlusMilliseconds(zoned.Offset.Milliseconds);
+    end;
+ end;
+{$ENDIF}
+
+end.
--- ProductVersion.pas
+++ ProductVersion.pas
@@ -5,8 +5,8 @@
 uses
   system.Reflection;
 
-[assembly: AssemblyVersion('1.0.0')]
-[assembly: AssemblyFileVersion('1.0.0')]
+[assembly: AssemblyVersion('1.1.0')]
+[assembly: AssemblyFileVersion('1.1.0')]
 [assembly: AssemblyCopyright('Copyright © 2023')]
 [assembly: AssemblyCompany('Moshine')]
 [assembly: AssemblyProduct('Moshine Foundation')]
