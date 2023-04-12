3028f3883854102e42b4e4addaf289571d2e53f2
John Moshakis <john@moshakis.com>
2023-04-11 22:27:19 -0400

    move around

---
+++ Moshine.Foundation.Contracts/IConnectionBuilder.pas
@@ -0,0 +1,15 @@
+﻿namespace Moshine.Foundation;
+
+{$IF ECHOES}
+
+uses
+  System.Data.Common;
+
+type
+  IConnectionBuilder = public interface
+    method Build:DbConnection;
+  end;
+
+{$ENDIF}
+
+end.
---
+++ Moshine.Foundation/ProductVersion.pas
@@ -0,0 +1,26 @@
+﻿namespace Moshine.Foundation;
+
+{$IFDEF ECHOES}
+
+uses
+  System.Resources,
+  System.Runtime.InteropServices,
+  System.Reflection;
+
+[assembly: AssemblyVersion('1.2.0')]
+[assembly: AssemblyFileVersion('1.2.0')]
+[assembly: AssemblyCopyright('Copyright © 2023')]
+[assembly: AssemblyCompany('Moshine')]
+[assembly: AssemblyProduct('Moshine Foundation')]
+
+[assembly: AssemblyTitle('')]
+[assembly: AssemblyDescription('')]
+[assembly: AssemblyConfiguration('')]
+[assembly: AssemblyTrademark('')]
+[assembly: AssemblyCulture('')]
+[assembly: NeutralResourcesLanguage('')]
+[assembly: ComVisible(false)]
+
+{$ENDIF}
+
+end.
