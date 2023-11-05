dad151f0218098cc0772a81ca43b3467c0699897
John Moshakis <john@moshakis.com>
2023-11-05 10:30:05 -0500

    Update ProductVersion.pas

--- Moshine.Foundation/ProductVersion.pas
+++ Moshine.Foundation/ProductVersion.pas
@@ -7,8 +7,8 @@ uses
   System.Runtime.InteropServices,
   System.Reflection;
 
-[assembly: AssemblyVersion('4.0.4')]
-[assembly: AssemblyFileVersion('4.0.4')]
+[assembly: AssemblyVersion('4.1.0')]
+[assembly: AssemblyFileVersion('4.1.0')]
 [assembly: AssemblyCopyright('Copyright © 2023')]
 [assembly: AssemblyCompany('Moshine')]
 [assembly: AssemblyProduct('Moshine Foundation')]
